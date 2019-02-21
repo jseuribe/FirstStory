a75d762919e604b734bae8327226eebdcd1cea70
jseuribe <jseuribe@hotmail.com>
2019-02-20 19:40:11 -0500

    began fiddling with room object presentation

--- firststory-front/src/app/roomobject/roomobject.component.ts
+++ firststory-front/src/app/roomobject/roomobject.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { RoomComponent } from '../room/room.component';
 
 @Component({
   selector: 'app-roomobject',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roomobject.component.scss']
 })
 export class RoomobjectComponent implements OnInit {
+
+  @Input() room: RoomComponent;
 
   constructor() { }
 
